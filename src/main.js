c40e3e826052b1336ddb1617a3a094b27febbe33
bo.ma <bo.ma@ailivingtech.com>
2020-08-20 17:28:08 +0800

    improve demo

--- src/main.js
+++ src/main.js
@@ -29,17 +29,19 @@ import './plugin.js';
 
 	function createDom(i) {
 		return `
-		<video id="player${i}" height="${height}" width="${width}" class="video-js vjs-big-play-centered"
-		 controls
-		 ish265
-		 hasvideo
-		 hasaudio
-		 lib="/node_modules/wx-inline-player-new/example/"
-		>`
-			+ `<source src="https://ks3-cn-beijing.ksyun.com/ksplayer/h265/outside_demo/v1.1.3/720P2M30fpsh265-wasmtest.flv" type='video/x-flv-h265' />`
+		<video id="player${i}" height="${height}" width="${width}" class="video-js vjs-big-play-centered"`
+		 +`controls `
+		//  +`autoplay `
+		 +`loop `
+		 +`ish265 `
+		 +`hasvideo `
+		 +`hasaudio `
+		 +`lib="/node_modules/wx-inline-player-new/example/" `
+		+`>`
+			// + `<source src="https://ks3-cn-beijing.ksyun.com/ksplayer/h265/outside_demo/v1.1.3/720P2M30fpsh265-wasmtest.flv" type='video/x-flv-h265' />`
 			// + `<source src="https://liveplay.xiaoeknow.com/live/5060_dKZTfCE6z2t6ecSX.flv" type='video/x-flv-h265'/>`
-			// + `<source src="http://localhost:8080/src/5s_265.flv" type='video/x-flv'/>`
-			// + `<source src="http://localhost:8080/src/5s_264.flv" type='video/x-flv'/>`
+			+ `<source src="src/5s_265.flv" type='video/x-flv'/>`
+			// + `<source src="src/5s_264.flv" type='video/x-flv'/>`
 		+`</video>`;
 	}
 })();
